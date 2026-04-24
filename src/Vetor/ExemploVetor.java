5be5b4caf3a2aa3300e4f05333811c6345722657
Alanis Santos <alanisosantos@outlook.com>
2026-04-24 15:03:17 -0300

    Vetor e Collections

---
+++ src/Vetor/ExemploVetor.java
@@ -0,0 +1,17 @@
+package Vetor;
+
+public class ExemploVetor {
+
+	public static void main(String[] args) {
+		
+		String vetorCachorros [] = {"Boxer", "Pastor Alemão", "Pinscher", "Husky Siberiano", "Corgi"};
+		
+		for (int numero = 0; numero < 5; numero++) {
+			System.out.println((numero + 1) +  "º elemento: " + vetorCachorros[numero]);
+			
+		}
+		
+		
+	}
+
+}
---
+++ src/Vetor/ExemploVetor2.java
@@ -0,0 +1,27 @@
+package Vetor;
+
+import java.util.Scanner;
+
+public class ExemploVetor2 {
+
+	public static void main(String[] args) {
+		
+		// Criando um vetor vazio com X posiçoes
+		int vetorNumeros[] = new int [3];
+		
+		Scanner leia = new Scanner(System.in);
+		
+		// Popular/adicionar itens ao vetor 
+		for(int indice = 0; indice < 3; indice ++) { 
+			System.out.println("Digite um número: ");
+			vetorNumeros[indice] = leia.nextInt();
+		}
+		
+		for(int indice = 0; indice < 3; indice ++) { 
+			System.out.println((indice + 1) +  "º elemento: " + vetorNumeros[indice]);
+		}
+		
+		System.out.println("Tamanho do Vetor: " + vetorNumeros.length);
+	}
+
+}
