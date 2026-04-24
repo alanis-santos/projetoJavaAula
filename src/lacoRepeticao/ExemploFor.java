6d14a5981545ac7d9dfd43040ef779d075e5799c
Alanis Santos <alanisosantos@outlook.com>
2026-04-24 08:43:33 -0300

    laço repetiçao

--- src/lacoRepeticao/ExemploFor.java
+++ src/lacoRepeticao/ExemploFor.java
@@ -6,17 +6,17 @@ public class ExemploFor {
 
 	public static void main(String[] args) {
 		
-	   int contador;
-	   String nome;
-       Scanner leia = new Scanner(System.in);
-        
-       for(contador = 0; contador < 3; contador++) {
-        
-       System.out.println("Digite o "  + contador + "º nome: ");
-       nome = leia.nextLine();
-       System.out.println("O " + contador + "º nome é: " +  nome + "\n");
+		int contador;
+		String nome;
+		Scanner leia = new Scanner(System.in);
+
+		for (contador = 1; contador < 4; contador++) {
+
+			System.out.println("Digite o " + contador + "º nome: ");
+			nome = leia.nextLine();
+			System.out.println("O " + contador + "º nome é: " + nome + "\n");
           
-         }
+       }
         
 
 	}
